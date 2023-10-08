f32b645959334854a4f5196a60eff2d21eb08db8
Gizem Candemir <gizem.candemir@marketcalculus.ai>
2023-10-08 20:34:56 +0300

    create animation with three js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useRef } from "react";
 import DisplaySection from "./components/DisplaySection";
 import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Nav";
 import SoundSection from "./components/SoundSection";
+import WebgiViewer from "./components/WebgiViewer";
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
   return (
     <div className="App">
-      <Navbar />
-      <Jumbotron />
-      <SoundSection />
-      <DisplaySection />
+      <div ref={contentRef} id="content">
+        <Navbar />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer  contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,168 @@
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+} from "react";
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  mobileAndTabletCheck,
+} from "webgi";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { scrollAnimation } from "../lib/scroll-animation";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const WebgiViewer = forwardRef((props, ref) => {
+  const canvasRef = useRef(null);
+  const [viewerRef, setViewerRef] = useState(null);
+  const [targetRef, setTargetRef] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [positionRef, setPositionRef] = useState(null);
+
+  const canvasContainerRef = useRef(null);
+  const [previewMode, setPreviewMode] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    triggerPreview() {
+      setPreviewMode(true);
+      canvasContainerRef.current.style.pointerEvents = "all";
+      props.contentRef.current.style.opacity = "0";
+      gsap.to(positionRef, {
+        x: 13.04,
+        y: -2.01,
+        z: 2.29,
+        duration: 2,
+        onUpdate: () => {
+          viewerRef.setDirty();
+          cameraRef.positionTargetUpdated(true);
+        },
+      });
+      gsap.to(targetRef, {
+        x: 0.11,
+        y: 0.0,
+        z: 0.0,
+        duration: 2,
+      });
+
+      viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: true });
+    },
+  }));
+
+  const memorizedScrollAnimation = useCallback((positon, target, onUpdate) => {
+    if (positon && target && onUpdate) {
+      scrollAnimation(positon, target, onUpdate);
+    }
+  }, []);
+
+  const setupViewer = useCallback(async () => {
+    const viewer = new ViewerApp({
+      canvas: canvasRef.current,
+    });
+    setViewerRef(viewer);
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+    const camera = viewer.scene.activeCamera;
+    const position = camera.position;
+    const target = camera.target;
+
+    setCameraRef(camera);
+    setPositionRef(position);
+    setTargetRef(target);
+
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(true));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
+    viewer.renderer.refreshPipeline();
+
+    await manager.addFromPath("scene-black.glb");
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+    window.scrollTo(0, 0);
+
+    let needsUpdate = true;
+
+    const onUpdate = () => {
+      needsUpdate = true;
+      viewer.setDirty();
+    };
+    viewer.addEventListener("preFrame", () => {
+      if (needsUpdate) {
+        camera.positionTargetUpdated(true);
+        needsUpdate = false;
+      }
+    });
+    memorizedScrollAnimation(position, target, onUpdate);
+  }, []);
+
+  useEffect(() => {
+    setupViewer();
+  }, []);
+
+  const handleExit = useCallback(() => {
+    canvasContainerRef.current.style.pointerEvents = "none";
+    props.contentRef.current.style.opacity = "1";
+    viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+    setPreviewMode(false);
+    
+    gsap.to(positionRef, {
+        x: 1.56,
+        y: 5.0,
+        z: 0.011,
+        scrollTrigger: {
+          trigger: ".display-section",
+          start: "top bottom",
+          end: "top top",
+          scrub: 2,
+          immediateRender: false,
+        },
+        onUpdate: () => {
+          viewerRef.setDirty();
+          cameraRef.positionTargetUpdated(true);
+        },
+      })
+
+      gsap.to(targetRef, {
+        x: -0.5,
+        y: 0.32,
+        z: 0.0,
+        scrollTrigger: {
+          trigger: ".display-section",
+          start: "top bottom",
+          end: "top top",
+          scrub: 2,
+          immediateRender: false,
+        },
+      });
+  }, [canvasContainerRef, viewerRef, positionRef, cameraRef, targetRef]);
+
+  return (
+    <div id="webgi-canvas-container" ref={canvasContainerRef}>
+      <canvas id="webgi-canvas" ref={canvasRef} />
+      {previewMode && (
+        <button className="button" onClick={handleExit}>
+          Exit
+        </button>
+      )}
+    </div>
+  );
+});
+
+export default WebgiViewer;
